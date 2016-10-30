ca5b4036b64736d98920b70a9b16da751946fdcd
JonathanGrant <jonathanallengrant@gmail.com>
2016-10-29 22:24:10 -0700

    new article and better algo

--- scripts.js
+++ scripts.js
@@ -24,13 +24,15 @@ var Sky = React.createClass({
 
 var Words = React.createClass({
     getInitialState: function() {
+        var articleTxt = "PORTLAND, Ore. – Reactions to the not-guilty verdict in the trial of seven people who occupied a national wildlife refuge in eastern Oregon has spread to Craigslist. A prank post listed the Malheur National Wildlife Refuge building for rent. The price: $0. “Quaint, stone cottage in beautiful, country setting!” The post said. “3 bed, 1 bath (3 if you count 2 latrines dug out by our last tenants) with spacious patio and breathtaking views!” The post said the refuge is available now and suggested calling the FBI to schedule a viewing. “If no one is there for the open house you can probably just stay,” it said. The poster also brought up the criticism that if the occupiers had been a race other than white, the situation could have ended differently. “All races welcome to apply! But can only guarantee safety for you-know-which-one (wink wink).” Seven of the occupiers who took over the refuge in early January were acquitted on all charges except one on Thursday. The announcement stunned many and supporters of the occupiers celebrated Thursday and Friday in downtown Portland. "
+        var words = articleTxt.split(" ")
         return {
-            words: ['Hey', 'welcome', 'to', 'SpeedVreadr', 'the', 'Virtual', 'Reality', 'speed', 'reader.', 'Hey', 'welcome', 'to', 'SpeedVreadr', 'the', 'Virtual', 'Reality', 'speed', 'reader.', 'Hey', 'welcome', 'to', 'SpeedVreadr', 'the', 'Virtual', 'Reality', 'speed', 'reader.', 'Hey', 'welcome', 'to', 'SpeedVreadr', 'the', 'Virtual', 'Reality', 'speed', 'reader.', 'Hey', 'welcome', 'to', 'SpeedVreadr', 'the', 'Virtual', 'Reality', 'speed', 'reader.'],
+            words: words,
             currentWord: 0
         }
     },
     timeForCurrentWord: function() {
-        return (10 * this.state.words[this.state.currentWord].length) + 250
+        return (30 * this.state.words[this.state.currentWord].length) + 80
     },
     componentDidMount: function() {
         setTimeout(this.nextWord, this.timeForCurrentWord())
